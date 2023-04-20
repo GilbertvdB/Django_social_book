589c852ef26266735c463ceba834078548ee79c6
gb <gvanderbiezen@gmail.com>
2023-04-20 13:03:42 +0200

    add settings method to views.py add settings route to urls.py edit settings.html with needed form edit signup method to redirect user to settings view after sign up.

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -13,6 +13,11 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'setting.html')
+
+
 def signup(request):
 
     if request.method == 'POST':
@@ -33,12 +38,14 @@ def signup(request):
                 user.save()
 
                 #log user in and redirecit to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Password does not match.')
@@ -70,5 +77,8 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+
+
 
 
